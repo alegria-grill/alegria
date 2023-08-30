9aff7bd2a8bc62f6fb5f23f459bffaf364ee3373
da99 <spam.i.hate@miniuni.com>
2023-08-29 19:05:29 -0500

    Typos.

--- src/lobby-tv/bashrc.sh
+++ src/lobby-tv/bashrc.sh
@@ -1,6 +1,9 @@
 
 # sudo raspi-config
 # source /apps/alegria/src/lobby-tv/bashrc.sh
-if [ -z "${DISPLAY}" ] && ! pgrep -x fbi && test "$SHLVL" -lt 1 ; then
-  exec /apps/alegria/sh/run lobby-tv
+set -x
+if [ -z "$DISPLAY" ] && ! pgrep -x fbi && test "$SHLVL" -lt 2 ; then
+  { /apps/alegria/sh/run lobby-tv && echo "=== Slideshow has ended. ==="  || echo "!!! Done with exit status: $?"
 fi
+set +x
+
