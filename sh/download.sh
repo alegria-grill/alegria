24f0b5eefe7dfcb1866187d5203fa7ae148c2887
da99 <spam.i.hate@miniuni.com>
2023-09-15 15:39:19 -0500

    Switching back to a desktop to play videos.

---
+++ sh/download.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+#
+
+set -x -u -e
+PATH="/apps/alegria/sh:$PATH"
+y "https://www.youtube.com/watch?v=UYMmtEFhuxA"
+
+y "https://www.youtube.com/watch?v=750nkDg9XPI"
+
+y "https://www.youtube.com/watch?v=2VuLBYrgG94"
+
+y "https://www.youtube.com/watch?v=hzv6NvPW33Q"
