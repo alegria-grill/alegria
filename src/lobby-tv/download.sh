5aba1e556f29a8127149424c5434a7221d3edeb7
da99 <spam.i.hate@miniuni.com>
2023-09-18 15:33:26 -0500

    Development checkpoint.

--- src/lobby-tv/download.sh
+++ src/lobby-tv/download.sh
@@ -3,10 +3,10 @@
 
 set -x -u -e
 PATH="/apps/alegria/sh:/apps/alegria/src/lobby-tv:$PATH"
-y "https://www.youtube.com/watch?v=UYMmtEFhuxA"
+y --skip-download "https://www.youtube.com/watch?v=UYMmtEFhuxA"
 
-y "https://www.youtube.com/watch?v=750nkDg9XPI"
+y --skip-download "https://www.youtube.com/watch?v=750nkDg9XPI"
 
-y "https://www.youtube.com/watch?v=2VuLBYrgG94"
+y --skip-download "https://www.youtube.com/watch?v=2VuLBYrgG94"
 
-y "https://www.youtube.com/watch?v=hzv6NvPW33Q"
+y --skip-download "https://www.youtube.com/watch?v=hzv6NvPW33Q"
