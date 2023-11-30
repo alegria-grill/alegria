0a23b3977f5a70b11109740ed1c0de6cfaa76ecd
da99 <spam.i.hate@miniuni.com>
2023-11-30 16:37:32 -0600

    Added: seasonal

---
+++ src/test.rb
@@ -0,0 +1,8 @@
+
+require "./src/File_Loop"
+
+
+
+photo_ext  = %[ -type f -iname '*.jpg' -or -iname '*.png' -or -iname '*.heic' ]
+office     = File_Loop.new(%[find "/apps/slides/exports" -maxdepth 1 #{photo_ext} ])
+puts office.list.inspect
