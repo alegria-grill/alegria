89c5a12ed32568958c55b03213d9042fac22c84a
da99 <spam.i.hate@miniuni.com>
2023-12-04 07:06:07 -0600

    Development checkpoint.

--- src/accident.sign.rb
+++ src/accident.sign.rb
@@ -1,15 +1,14 @@
-
-LAST_ACCIDENT_TXT = "data/last.accident.timestamp"
-TEMPLATES_GREEN = "templates/accident.green.svg"
-TEMPLATES_YELLOW = "templates/accident.yellow.svg"
-IMAGES_ACCIDENT = "/tmp/accident.svg"
+LAST_ACCIDENT_TXT = 'data/last.accident.timestamp'
+TEMPLATES_GREEN = 'templates/accident.green.svg'
+TEMPLATES_YELLOW = 'templates/accident.yellow.svg'
+IMAGES_ACCIDENT = '/tmp/accident.svg'
 NOW_TS = Time.now.to_i
 NOW = Time.now
 TODAY = Time.new(NOW.year, NOW.month, NOW.day, 0, 0, 0)
 
-new_cmd = ARGV.join " "
+new_cmd = ARGV.join ' '
 case new_cmd
-when "reset"
+when 'reset'
   File.write(LAST_ACCIDENT_TXT, NOW_TS.to_s)
 
 when /write\ +\d+/
@@ -17,24 +16,23 @@ when /write\ +\d+/
   new_ts = TODAY.to_i - (24 * 60 * 60 * days)
   File.write(LAST_ACCIDENT_TXT, new_ts)
 
-when "update"
+when 'update'
   new_count = begin
-                secs = Time.now.to_i - File.read(LAST_ACCIDENT_TXT).to_i
-                days = (secs/60/60/24).to_i
-                (days < 1) ? 0 : days
-              end
-  tmpl = (new_count < 3) ? TEMPLATES_YELLOW : TEMPLATES_GREEN
-  plural = (new_count == 1) ? "" : "s"
+    secs = Time.now.to_i - File.read(LAST_ACCIDENT_TXT).to_i
+    days = (secs / 60 / 60 / 24).to_i
+    days < 1 ? 0 : days
+  end
+  tmpl = new_count < 3 ? TEMPLATES_YELLOW : TEMPLATES_GREEN
+  plural = new_count == 1 ? '' : 's'
   File.write(
     IMAGES_ACCIDENT,
-    File.read(tmpl).sub("!D", new_count.to_s).sub("!S", plural)
+    File.read(tmpl).sub('!D', new_count.to_s).sub('!s', plural)
   )
   puts "Updated sign to read: #{new_count} days"
   puts "File: #{IMAGES_ACCIDENT}"
 
 else
-  STDERR.puts "!!! Unknown arguments: #{new_cmd.inspect}"
+  warn "!!! Unknown arguments: #{new_cmd.inspect}"
   exit 1
 
 end # case
-
