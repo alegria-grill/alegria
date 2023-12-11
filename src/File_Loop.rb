c3123c6b2c80ff950af8fefd3221d9864d8e6204
da99 <spam.i.hate@miniuni.com>
2023-12-11 10:27:24 -0600

    Added: expire files by hour and minute.

--- src/File_Loop.rb
+++ src/File_Loop.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'date'
 
 class File_Loop
-  attr_reader :i, :list
+  attr_reader :i, :list, :cmd
 
   MONTHS = Date::ABBR_MONTHNAMES.map { |x| x && x.upcase }
-  HUMAN_DATE_PATTERN = /[a-z]{3,4}_\d{2}_\d{4}\..{1,4}$/i
+  HUMAN_DATE_PATTERN = /[a-z]{3,4}_\d{2}_\d{4}(_\d{2}_\d{2})?\..{1,4}$/i.freeze
 
   class << self
-    def month_name_to_month_number(s)
-      i = File_Loop::MONTHS.index(s.upcase) || 0
+    def month_name_to_month_number(str)
+      i = File_Loop::MONTHS.index(str.upcase) || 0
       return "0#{i}" if i < 10
 
       i
@@ -18,27 +20,28 @@ class File_Loop
       result = s.upcase[HUMAN_DATE_PATTERN]
       return nil unless result
 
-      s_month, s_day, s_year = result.split('.').first.split('_')
+      s_month, s_day, s_year, s_hr, s_min = result.split('.').first.split('_')
       i_month = month_name_to_month_number(s_month)
-      "#{s_year}#{i_month}#{s_day}".to_i
+      "#{s_year}#{i_month}#{s_day}#{s_hr || '00'}#{s_min || '00'}".to_i
     end
 
     def has_date_ending?(str)
       !!str[HUMAN_DATE_PATTERN]
     end
+
+    def get_files(s_cmd)
+      today = File_Loop.date_string_to_number("#{Time.now.strftime('%b_%d_%Y_%H_%M')}.now")
+      `#{s_cmd}`.strip.split("\n").select do |file|
+        f_number = File_Loop.date_string_to_number(file)
+        f_number ? (today < f_number) : true
+      end
+    end
   end # class self
 
   def initialize(cmd)
     @i = 0
-    today = File_Loop.date_string_to_number("#{Time.now.strftime('%b_%d_%Y')}.now")
-    @list = `#{cmd}`.strip.split("\n").select do |file|
-      f_number = File_Loop.date_string_to_number(file)
-      if f_number
-        today < f_number
-      else
-        true
-      end
-    end
+    @cmd = cmd
+    @list = self.class.get_files(cmd)
   end # def
 
   def next
@@ -46,7 +49,10 @@ class File_Loop
 
     photo = @list[@i]
     @i += 1
-    @i = 0 if @i > (@list.size - 1)
+    if end?
+      @list = self.class.get_files(@cmd)
+      @i = 0
+    end
     photo
   end
 
--- src/test.rb
+++ src/test.rb
@@ -1,8 +1,6 @@
 
 require "./src/File_Loop"
 
-
-
-photo_ext  = %[ -type f -iname '*.jpg' -or -iname '*.png' -or -iname '*.heic' ]
-office     = File_Loop.new(%[find "/apps/slides/exports" -maxdepth 1 #{photo_ext} ])
-puts office.list.inspect
+photo_ext  = %( -type f -iname '*.jpg' -or -iname '*.png' -or -iname '*.heic' )
+office     = File_Loop.new(%( find "/apps/slides/exports" -maxdepth 1 #{photo_ext} ))
+office.list.each { |x| puts x }
