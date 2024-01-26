5f202f1c38935b28942ecf5b246b9fa227e904b1
da99 <spam.i.hate@miniuni.com>
2024-01-26 12:28:08 -0600

    Development checkpoint.

--- src/test.rb
+++ src/test.rb
@@ -1,6 +1,6 @@
 
 require "./src/File_Loop"
 
-ext  = %( -type f -iname '*.mp4' )
+ext  = %( -type f -iname '*.mp4' -or -iname '*.mkv' )
 office = File_Loop.new(%( find "/home/pi/Videos/mr_safety" -maxdepth 1 #{ext} ))
 office.list.each { |x| puts x }
