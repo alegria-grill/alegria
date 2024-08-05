425aaa3ad2fa27a8db24c5b32df14e08ee6852a2
da99 <spam.i.hate@miniuni.com>
2024-08-05 07:39:13 -0500

    Added: Birthday.Sign.rb

---
+++ src/Birthday.Sign.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'date'
+
+module Birthday_Sign
+  extend self
+
+  def current_month
+    today = Time.now
+    month = Date::MONTHNAMES[today.month].downcase
+  end
+
+  def current_template_file
+    "/apps/pictures/templates/birthday.#{current_month}.svg"
+  end
+
+  def current_file
+    "/tmp/birthday.#{current_month}.svg"
+  end
+
+  def generate
+    return false unless File.exist?(current_template_file)
+    content = File.read(current_template_file)
+    today = Time.now
+    day = today.day
+
+    (day - 1).times do |i|
+      id = "day#{i + 1}"
+      content = content.sub(%{id="#{id}"}, %{id="#{id}" style="opacity:30%"})
+    end
+
+    File.write(current_file, content)
+    current_file
+  end
+end # module
+
+
+if $PROGRAM_NAME == __FILE__
+  cmd = ARGV.join(' ')
+  case cmd
+  when "generate"
+    result = Birthday_Sign.generate
+    if result
+      puts result
+    else
+      warn "!!! Something went wrong. File did not generate."
+      exit 1
+    end
+  else
+    warn "!!! Unknown command: #{cmd}"
+    exit 1
+  end
+end # if
